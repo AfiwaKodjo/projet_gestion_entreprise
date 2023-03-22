83a0f536ee1cf2a9b3cad85442423728e0f7d18f
Afiwa Kodjo <99075561+AfiwaKodjo@users.noreply.github.com>
2023-03-22 10:24:54 +0000

    Delete gestion_entreprise_web directory

--- gestion_entreprise_web/DjangoAPI/livreur/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-#Create your models here.
-"""class User(AbstractUser):
-    nom = models.CharField(max_length=100)
-    mdp = models.CharField(max_length=25, unique=True)
-    
-    REQUIRED_FIELDS=['nom','mdp']"""
--- gestion_entreprise_web/gestion_entreprise_web/gestion_entreprise_web/gestion_entreprise/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestionEntrepriseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestion_entreprise'
