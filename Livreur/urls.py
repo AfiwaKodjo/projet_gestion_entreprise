8739bc4800b450566d6d10d37542e64b4eea48e6
Afiwa Kodjo <99075561+AfiwaKodjo@users.noreply.github.com>
2023-03-22 11:17:14 +0000

    Add files via upload

---
+++ Livreur/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from fact_app.views import RegisterView 
+from .views import LivreurView, LoginView, LogoutView, RegisterView
+urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('livreur', LivreurView.as_view()),
+    path('logout', LogoutView.as_view()),
+
+
+    
+]
